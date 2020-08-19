9008473d1cf9b4c29f64c863f6884050dc3790ad
Nathan <43883343+NathanCrowley@users.noreply.github.com>
2020-08-19 01:05:15 +0100

    Delete 4.py

--- Python_Challanges/Easy/4.py
+++
@@ -1,7 +0,0 @@
-import math
-def Volume_of_Cone(height,radius):
-    #return to nearest 100th
-    volume = ((math.pi*(radius**2))*height/3)
-    return "Volume = {:0.2f}".format(volume)
-
-print(Volume_of_Cone(10,5))
